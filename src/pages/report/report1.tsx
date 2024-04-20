aaa68078e4b5d87f876bbb416faf2212a7108eaa
ParkJaemin <nm890805@naver.com>
2024-04-20 23:36:06 +0900

    MAC 기관보고서 페이지 추가

---
+++ src/pages/report/report1.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Aside from '../../components/aside'
+import Searchbox from '../../components/searchbox';
+
+function Report1 () {
+
+  return (
+    <div className="wrap">
+      <Aside />
+      <div className='contbody'>
+        <div className="page-title with-btn">
+          기관보고서
+          <a href="#!" className='btn'>
+            <i className='ico arrow_left'></i>
+            뒤로가기
+          </a>
+        </div>
+        <div className="body-inner">
+          <div className="report_wrap">
+            <Searchbox />
+            <div className="box_outer mt20">
+              <p className="box_title">분석결과</p>
+              <div className="info_box">
+                <i className="ico smile ico-size-50"></i>
+                <p>검색된 기관이 없어요.</p>
+              </div>
+            </div>
+            <div className="box_outer mt20">
+              <div className="box_title_wrap">
+                <p>분석결과</p>
+                <div className="btn_wrap">
+                  <button className='btn type-line hover'>결과지 다운로드(pdf)</button>
+                  <button className='btn type-line hover'>데이터 다운로드(excel)</button>
+                </div>
+              </div>
+              <div className="info_box">
+                <i className="ico smile ico-size-50"></i>
+                <p>검색된 기관이 없어요.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Report1
